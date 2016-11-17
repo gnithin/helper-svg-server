dd98f566def7d9cf43436dca09e60614c8ce3d2a
Nithin <nithin.g@ignitesol.com>
2016-11-17 17:42:59 +0530

    [parse-req] Add the sunny day scenario for parsing the requests

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
 from lib.bottle import Bottle, request
 import logging
-from dal import DAL
+# from dal import DAL
 from utils import Utils
+from pprint import pprint
 
 bottle = Bottle()
 
@@ -50,3 +51,67 @@ def stats():
 
     # Some kind of cleansing is required here
     return Utils.resp_json(1, "success", contents=final_content)
+
+if __name__ == "__main__":
+    # Just testing out the parser bit
+    ip_str = '''
+    === RUN   TestBST_integer
+    --- PASS: TestBST_integer (0.00s)
+    === RUN   TestBST_string
+    --- PASS: TestBST_string (0.00s)
+    === RUN   TestBST_customObj
+    --- PASS: TestBST_customObj (0.00s)
+    === RUN   TestHeap_loadTesting
+    --- PASS: TestHeap_loadTesting (0.00s)
+    === RUN   TestHeap_integer
+    --- PASS: TestHeap_integer (0.00s)
+    === RUN   TestHuffman_basic
+    --- PASS: TestHuffman_basic (0.00s)
+    === RUN   TestHuffman_lyrics
+    --- PASS: TestHuffman_lyrics (0.00s)
+    === RUN   TestTrie_development
+    --- PASS: TestTrie_development (0.00s)
+    === RUN   TestTrie_multiple
+    --- PASS: TestTrie_multiple (0.00s)
+    === RUN   TestTrie_withOptionsMap
+    --- PASS: TestTrie_withOptionsMap (0.00s)
+    === RUN   TestTrie_stopWords
+    --- PASS: TestTrie_stopWords (0.00s)
+    === RUN   TestTrie_Insertion
+    --- PASS: TestTrie_Insertion (1.86s)
+        Trie_test.go:143: 904061
+    PASS
+    BenchmarkExp_map-2           	50000000	        27.4 ns/op
+    BenchmarkExp_ptr-2           	2000000000	         0.65 ns/op
+    BenchmarkExp_arr-2           	2000000000	         0.65 ns/op
+    BenchmarkMaxHeap_ins_50000-2 	 5000000	       287 ns/op
+    BenchmarkMaxHeap_ins_90000-2 	 3000000	       412 ns/op
+    BenchmarkMaxHeap_ins_100000-2	 2000000	       521 ns/op
+    BenchmarkMaxHeap_ins_200000-2	       1	1176492043 ns/op
+    BenchmarkMaxHeap_ins_300000-2	       1	1858822997 ns/op
+    BenchmarkMaxHeap_ins_400000-2	       1	2388464427 ns/op
+    BenchmarkMaxHeap_ins_500000-2	       1	3239821271 ns/op
+    BenchmarkMinHeap_ins_50000-2 	 5000000	       279 ns/op
+    BenchmarkMinHeap_ins_90000-2 	 3000000	       375 ns/op
+    BenchmarkMinHeap_ins_100000-2	 3000000	       394 ns/op
+    BenchmarkMinHeap_ins_200000-2	       1	1034617725 ns/op
+    BenchmarkMinHeap_ins_300000-2	       1	1623952866 ns/op
+    BenchmarkMinHeap_ins_400000-2	       1	2059533534 ns/op
+    BenchmarkMinHeap_ins_500000-2	       1	2796894245 ns/op
+    BenchmarkMaxHeap_pop5000-2   	  100000	     15009 ns/op
+    BenchmarkMaxHeap_pop20000-2  	   20000	     65800 ns/op
+    BenchmarkMaxHeap_pop50000-2  	    5000	    222388 ns/op
+    BenchmarkMaxHeap_pop90000-2  	    2000	    603784 ns/op
+    BenchmarkMaxHeap_pop100000-2 	    1000	   1031251 ns/op
+    BenchmarkMaxHeap_pop200000-2 	       1	1477133854 ns/op
+    BenchmarkMaxHeap_pop300000-2 	       1	2303676884 ns/op
+    BenchmarkMaxHeap_pop400000-2 	       1	3039391517 ns/op
+    BenchmarkMaxHeap_pop500000-2 	       1	4007572221 ns/op
+    Benchmark_trieInsertion-2    	       1	1906820476 ns/op
+    Benchmark_triePassSearch-2   	       1	1829905968 ns/op
+    Benchmark_strPassSearch-2    	    1000	   1694059 ns/op
+    Benchmark_trieFailSearch-2   	       1	1817361659 ns/op
+    Benchmark_strFailSearch-2    	     500	   3688744 ns/op
+    ok  	github.com/gnithin/gotree/tree/tests	124.664s
+    '''
+    pprint(Utils.parse_content(ip_str, ""))
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
 import json
 from datetime import datetime
+import re
+# import logging
+from pprint import pprint
 
 
 class Utils:
@@ -11,10 +14,32 @@ class Utils:
 
     @classmethod
     def parse_content(cls, content, timestamp):
-        return {
-            "content": content,
-            "timestamp": timestamp
-        }
+        content = content.strip()
+        final_resp = {}
+
+        test_constituents = re.split(
+            r'^\s*(?:PASS|FAIL)',
+            content,
+            flags=re.MULTILINE
+        )
+        pprint(test_constituents)
+
+        if len(test_constituents) == 2:
+            tests, benchmarks = test_constituents
+            tests_results = cls.parse_tests(tests)
+            bm_results = cls.parse_benchmarks(benchmarks)
+
+            print("Test and Benchmarks - ")
+            pprint(tests_results)
+            pprint(bm_results)
+
+            final_resp["tests"] = tests_results
+            final_resp["bm"] = bm_results
+        else:
+            print("Fail: Number of consitituents - " +
+                  str(len(test_constituents)))
+
+        return final_resp
 
     @classmethod
     def json_serial(cls, obj):
@@ -23,3 +48,42 @@ class Utils:
             serial = obj.isoformat()
             return serial
         raise TypeError("Type not serializable")
+
+    @classmethod
+    def parse_tests(cls, tests):
+        tests = tests.strip(" \n")
+
+        # Get the test results
+        tests_list = tests.split("\n")
+        all_test_data = []
+        for i in range(0, len(tests_list) - 1, 2):
+            run_line = tests_list[i].strip(" \n")
+            status_line = tests_list[i+1].strip(" \n")
+
+            test_name = run_line.split(" ")[-1]
+            status_split = status_line.split(" ")
+            status = status_split[1].strip(":")
+            time_taken = status_split[-1].strip("()")
+            all_test_data.append({
+                "test_name": test_name,
+                "status": status,
+                "time_taken": time_taken
+            })
+
+        return all_test_data
+
+    @classmethod
+    def parse_benchmarks(cls, bm):
+        bm = bm.strip(" \n")
+
+        all_bm_data = []
+        for bm_line in bm.split('\n'):
+            bm_line = bm_line.strip(" \n")
+            bm_contents = re.split(r'[\t\s\n]{2,}', bm_line)
+            if len(bm_contents) >= 3:
+                all_bm_data.append({
+                    "bm_name" : bm_contents[0],
+                    "iterations": bm_contents[1],
+                    "rate": bm_contents[2]
+                })
+        return all_bm_data
