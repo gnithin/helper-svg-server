72606b583af18b1ab81a365f7584062ca3b554ce
Nithin <nithin.g@ignitesol.com>
2016-11-18 17:03:29 +0530

    [svg] Add templating facility

--- utils.py
+++ utils.py
@@ -3,6 +3,16 @@ from datetime import datetime
 import re
 import logging
 # from pprint import pprint
+import jinja2
+import os
+
+CONFIG_TEMPLATE_PATH = "/templates/"
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) +
+                                   CONFIG_TEMPLATE_PATH),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True
+)
 
 
 class Utils:
@@ -102,3 +112,40 @@ class Utils:
             serial = obj.isoformat()
             return serial
         raise TypeError("Type not serializable")
+
+    @classmethod
+    def get_template_from_dir(cls, file_name, dir_path, **kwargs):
+        if not file_name:
+            logging.info("No file name specified for the template")
+            return False
+
+        if not dir_path:
+            dir_path = CONFIG_TEMPLATE_PATH
+
+        # Setting this everytime
+        JINJA_ENVIRONMENT.loader = jinja2.FileSystemLoader(
+            os.path.dirname(__file__) + dir_path
+        )
+        logging.info(os.path.dirname(__file__) + dir_path)
+        try:
+            template = JINJA_ENVIRONMENT.get_template(file_name)
+            return template.render(**kwargs)
+        except Exception as e:
+            logging.info("Error while creating template - " + str(file_name))
+            logging.info(e.message)
+            logging.info(str(e))
+
+        return False
+
+    @classmethod
+    def get_template(cls, file_name, **kwargs):
+        '''Get template resultant string. '''
+        if not file_name:
+            logging.info("No file name specified for the template")
+            return False
+
+        return cls.get_template_from_dir(
+            file_name,
+            CONFIG_TEMPLATE_PATH,
+            **kwargs
+        )
